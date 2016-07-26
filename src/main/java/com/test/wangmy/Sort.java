01ed159280cc8524d8296451f3b2b5d410c54f3b
mingyu0519 <mingyu0519@gmail.com>
2016-07-26 15:31:07 +0800

    add selectSort

--- src/main/java/com/test/wangmy/Sort.java
+++ src/main/java/com/test/wangmy/Sort.java
@@ -2,30 +2,45 @@ package com.test.wangmy;
 
 public class Sort {
 	
-	public void maoPao() {
+	public void selectSort() {
 		int[] a = {1,2,3,6,9,80,123,22,344,6};
-		System.out.println("排序之前：");
+		int size = a.length, temp;
+		for (int i = 0; i < a.length; i++) {
+			int k = i;
+			for (int j = size - 1; j > i; j--) {
+				if (a[j] < a[k]) {
+					k = j;
+				}
+			}
+			temp = a[i];
+			a[i] = a[k];
+			a[k] = temp;
+		}
 		for (int i : a) {
 			System.out.print(i + " ");
 		}
-		
+	}
+	
+	public void bubbleSort() {
+		int[] a = {1,2,3,6,9,80,123,22,344,6};
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0; j < a.length- i - 1; j++) {
-				if (a[j] < a[j+1]) {
+				if (a[j] > a[j+1]) {
 					int temp = a[j];
 					a[j] = a[j+1];
 					a[j+1] = temp;
 				}
 			}
 		}
-		System.out.println("\n排序之后：");
 		for (int i : a) {
 			System.out.print(i + " ");
 		}
 	}
 	public static void main(String[] args) {
 		Sort sort = new Sort();
-		sort.maoPao();
+		sort.bubbleSort();
+		System.out.println();
+		sort.selectSort();
 	}
 
 }
