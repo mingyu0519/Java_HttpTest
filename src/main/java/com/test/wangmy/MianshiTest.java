f667567dcecec67928d65f0d85748bb8711a3bf5
Mingyu Wang <mingyu0519@gmail.com>
2016-07-31 21:54:57 +0800

    add file

---
+++ src/main/java/com/test/wangmy/MianshiTest.java
@@ -0,0 +1,10 @@
+package com.test.wangmy;
+
+public class MianshiTest {
+	public static void main(String[] args) {
+		int i = 5;
+		int j = 10;
+		System.out.println(i + ~j);
+		System.out.println(~j);
+	}
+}
