ab81103717a012afb0800c7b925879d3105ab676
mingyu0519 <mingyu0519@gmail.com>
2016-08-04 16:30:51 +0800

    add quick sort

--- src/main/java/com/test/cycle/BubbleSort.java
+++ src/main/java/com/test/cycle/BubbleSort.java
@@ -5,9 +5,11 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		int[] aList = {4,5,2,1,3};
 		int temp = 0;
+		// 遍历aList数组
 		for (int i = 0; i < aList.length - 1; i++) {
-			for (int j = 0; j < aList.length - i - 1; j++) {
+			for (int j = 0; j < aList.length-i-1; j++) {
 				if (aList[j] > aList[j+1]) {
+					// 交换数据
 					temp = aList[j];
 					aList[j] = aList[j+1];
 					aList[j+1] = temp;
--- src/main/java/com/test/cycle/QuickSort.java
+++ src/main/java/com/test/cycle/QuickSort.java
@@ -1,9 +1,37 @@
 package com.test.cycle;
 
 public class QuickSort {
+	private static void quick(int[] a){
+		if (a.length > 0) {
+			quickSort(a, 0, a.length-1);
+		}
+	}
+	
+	private static void quickSort(int[] a, int low, int heigh) {
+		if (low < heigh) {
+			int middle = getMiddle(a, low, heigh);
+		}
+	}
+	
+	private static int getMiddle(int[] a, int low, int heigh) {
+		int temp = a[low];
+		while (low < heigh) {
+			
+		}
+		return 1;
+	}
 
 	public static void main(String[] args) {
-		
+		int[] aList = {3,5,1,2,4};
+		System.out.println("排序之前－－－－－－－－－－－－－－");
+		for (int i : aList) {
+			System.out.print(i + " ");
+		}
+		quick(aList);
+		System.out.println("排序之后－－－－－－－－－－－－－－");
+		for (int i : aList) {
+			System.out.print(i + " ");
+		}
 	}
 
 }
