00b50edb73b461b55bff7ed47b77b4c10a681c0f
Mingyu Wang <mingyu0519@gmail.com>
2016-08-08 20:19:20 +0800

    fibonacci test2

--- src/main/java/com/test/algorithm/Fibonacci.java
+++ src/main/java/com/test/algorithm/Fibonacci.java
@@ -1,5 +1,7 @@
 package com.test.algorithm;
 
+import java.util.ArrayList;
+
 public class Fibonacci {
 	public static void test1(int n) {
 		int a = 1, b=1, c = 0;
@@ -16,9 +18,25 @@ public class Fibonacci {
 			}
 		}
 	}
+	
+	public static void test2(int n) {
+		int[] arrayList = new int[n];
+		arrayList[0] = arrayList[1] =1;
+		for (int i = 0; i < arrayList.length; i++) {
+			if (i == 0) {
+				System.out.println("第" + (i+1) + "等于" + arrayList[0]);
+			}else if (i == 1) {
+				System.out.println("第" + (i+1) + "等于" + arrayList[1]);
+			}else {
+				arrayList[i] = arrayList[i-1] +arrayList[i-2];
+				System.out.println("第" + (i+1) + "等于" + arrayList[i]);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		test1(20);
+//		test1(20);
+		test2(20);
 	}
 
 }
